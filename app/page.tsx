d32ba1506ea86e837732ddaa0a6ef42de16a89cc
Miguel de los Reyes <thisiswhataninjalookslike@gmail.com>
2025-03-17 11:16:57 -0400

    add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,70 @@
-import { Text } from "@mantine/core";
+"use client";
+
+import {
+  Button,
+  Center,
+  Container,
+  Stack,
+  Text,
+  TextInput,
+} from "@mantine/core";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+function parseUrl(url: string) {
+  const parsed = new URL(url);
+  console.log(parsed.hostname);
+  if (parsed.hostname.includes("aesops")) {
+    return ["aesops", parsed.pathname.split("/")[1]];
+  }
+  if (parsed.hostname.includes("tournaments.nullsignal")) {
+    return ["cobra", parsed.pathname.split("/")[2]];
+  }
+  return null;
+}
 
 export default function HomePage() {
-  return <Text>Home page</Text>;
+  const router = useRouter();
+  const [value, setValue] = useState("");
+  const [hasError, setHasError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <Container pt="40vh">
+      <Center>
+        <Stack>
+          <TextInput
+            label="URL"
+            value={value}
+            onChange={(e) => {
+              setHasError(false);
+              setValue(e.target.value);
+            }}
+            error={hasError}
+          />
+          <Button
+            variant="gradient"
+            gradient={{ from: "red", to: "yellow", deg: 30 }}
+            loading={loading}
+            onClick={() => {
+              let parsed = null;
+              try {
+                parsed = parseUrl(value);
+              } catch (error) {
+                setHasError(true);
+              }
+              if (parsed) {
+                setLoading(true);
+                router.push(`/${parsed[0]}/${parsed[1]}`);
+              } else {
+                setHasError(true);
+              }
+            }}
+          >
+            Go
+          </Button>
+        </Stack>
+      </Center>
+    </Container>
+  );
 }
