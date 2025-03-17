74e44fdb5010d989c741921702c7953caa24a9fe
Miguel de los Reyes <thisiswhataninjalookslike@gmail.com>
2025-03-17 19:50:05 -0400

    fix flashbang

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en" {...mantineHtmlProps}>
       <head>
-        <ColorSchemeScript />
+        <ColorSchemeScript defaultColorScheme="dark" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
