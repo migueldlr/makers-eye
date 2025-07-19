42b9977e121c514a57984314f3fd7affcce97702
Miguel de los Reyes <thisiswhataninjalookslike@gmail.com>
2025-07-19 11:30:43 -0400

    helper text on matchup table

--- components/stats/charts/MatchupTable/FirstRow.tsx
+++ components/stats/charts/MatchupTable/FirstRow.tsx
@@ -13,9 +13,32 @@ export default function FirstRow({
   mainSide: "runner" | "corp";
   showPercentages: boolean;
 }) {
+
+  const runnerWins = winrates.reduce((acc, winrate) => {
+    return acc + winrate.runner_wins;
+  }, 0);
+  const corpWins = winrates.reduce((acc, winrate) => {
+    return acc + winrate.corp_wins;
+  }, 0);
+
+  const [sideOneWins, sideTwoWins] =
+    mainSide === "runner"
+      ? [runnerWins, corpWins]
+      : [corpWins, runnerWins];
+  const hasResults = sideOneWins + sideTwoWins > 0;
+  const percentageDisplay = `${Math.round(
+    (sideOneWins / (sideOneWins + sideTwoWins)) * 100
+  )}%`;
   return (
     <TableTr>
-      <FillerTd count={2} />
+      <TableTd style={{ fontStyle: "italic" }}>All {mainSide}s</TableTd>
+      <TableTd>
+        {showPercentages
+          ? hasResults
+            ? percentageDisplay
+            : "-"
+          : `${sideOneWins}-${sideTwoWins}`}
+      </TableTd>
       {allSideTwoIds.filter(Boolean).map((sideTwoId) => {
         const gamesWithSideTwoId = winrates.filter(
           (winrate) =>
